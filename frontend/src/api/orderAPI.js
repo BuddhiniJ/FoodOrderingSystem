e93f0d830ebdfcbfcac215399a5c97fcf92673f6
IT22358684 <it22358684@my.sliit.lk>
2025-04-23 14:20:30 +0530

    Revert "Add backend and frontend files"
    This reverts commit f40f8421f786b64fa3b4792efe11367c3c625e62.

--- frontend/src/api/orderAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const orderAPI = axios.create({
-  baseURL: 'http://localhost:5002/api', // ORDER service backend
-});
-
-export default orderAPI;
--- frontend/src/api/restaurantAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const restaurantAPI = axios.create({
-  baseURL: 'http://localhost:5001/api', // RESTAURANT service backend
-});
-
-export default restaurantAPI;
